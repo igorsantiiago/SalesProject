01f6545b09603539ea7180c98fc7b16108e28a91
Igor Santiago <contato.igorsantiago@outlook.com>
2023-08-14 07:02:26 -0300

    Repositories implementation completed

---
+++ SalesProject.Domain.Infra/Repositories/ProductRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesProject.Domain.Infra.Data;
+using SalesProject.Domain.Models;
+using SalesProject.Domain.Queries;
+using SalesProject.Domain.Repositories;
+
+namespace SalesProject.Domain.Infra.Repositories;
+
+public class ProductRepository : IProductRepository
+{
+    private readonly SalesProjectDbContext _context;
+
+    public ProductRepository(SalesProjectDbContext context)
+    {
+        _context = context;
+    }
+
+    public void Create(Product product)
+    {
+        _context.Products.Add(product);
+        _context.SaveChanges();
+    }
+
+    public void Delete(Product product)
+    {
+        _context.Products.Remove(product);
+        _context.SaveChanges();
+    }
+
+    public IEnumerable<Product> GetAll() => _context.Products.AsNoTracking().ToList();
+
+    public Product GetById(Guid id)
+    {
+        Product? product = _context.Products.AsNoTracking().FirstOrDefault(ProductQueries.GetById(id));
+        return product!;
+    }
+
+    public IEnumerable<Product> GetByName(string name) => _context.Products.AsNoTracking().Where(ProductQueries.GetByName(name)).ToList();
+
+    public IEnumerable<Product> GetByTag(string tag) => _context.Products.AsNoTracking().Where(ProductQueries.GetByTag(tag)).ToList();
+
+    public void Update(Product product)
+    {
+        _context.Entry(product).State = EntityState.Modified;
+        _context.SaveChanges();
+    }
+}
